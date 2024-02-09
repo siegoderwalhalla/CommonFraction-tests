27acd10879744c3ddae604f6f18c0adc9089292a
box <pashayashik@mail.ru>
2024-02-09 16:49:39 +0300

    работа с классами

---
+++ src/CommonFraction.kt
@@ -0,0 +1,22 @@
+class CommonFraction(numerator: Int, denominator: Int) {
+    fun plus(secondFraction: CommonFraction): CommonFraction {
+
+        return CommonFraction(1, 2)
+    }
+
+    fun minus(secondFraction: CommonFraction): CommonFraction {
+        return
+
+    }
+
+    fun multiply(secondFraction: CommonFraction): CommonFraction {
+        return
+
+    }
+
+    fun divide(secondFraction: CommonFraction): CommonFraction {
+        return
+
+    }
+
+}
